e323317ab5b3092129e8aa0c207a5c59331d87bc
Bo Zhang (IT) <zhangbo5@CICC.GROUP>
2021-11-27 11:11:19 +0800

    init onlineService

---
+++ src/test/java/com/online/OnlineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
