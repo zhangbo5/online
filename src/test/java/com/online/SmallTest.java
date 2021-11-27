c800e310fc165857b220c28a061e15083230ab75
zhangbo5 <Zh1580126>
2021-11-27 11:33:41 +0800

    start with a SmallTest

---
+++ src/test/java/com/online/SmallTest.java
@@ -0,0 +1,15 @@
+package com.online;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: zhangbo5
+ * @DateTime: 2021/11/27 11:31
+ */
+public class SmallTest {
+
+    @Test
+    public void test(){
+        System.out.println("small test");
+    }
+}
